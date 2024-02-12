4595902deb952238749afdf8b245b65b6158e312
hammoh7 <soahammohaadkar@gmail.com>
2024-02-13 02:58:02 +0530

    App-layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ConvexClientProvider } from "../providers/convex-client";
+import { ConvexClientProvider } from "@/providers/convex-client";
 
 const inter = Inter({ subsets: ["latin"] });
 
