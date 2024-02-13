7df03da3f7fd099bd475b4a98b90ca70c2899347
hammoh7 <soahammohaadkar@gmail.com>
2024-02-14 00:43:00 +0530

    Components

---
+++ components/label.tsx
@@ -0,0 +1,37 @@
+import { TooltipTrigger } from "@radix-ui/react-tooltip";
+import { Tooltip, TooltipContent, TooltipProvider } from "./ui/tooltip";
+
+export interface LabelProps {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+  alignOffset?: number;
+}
+
+export const Label = ({
+  label,
+  children,
+  side,
+  align,
+  sideOffset,
+  alignOffset,
+}: LabelProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={100}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent
+          className="text-white bg-black border-black"
+          side={side}
+          align={align}
+          sideOffset={sideOffset}
+          alignOffset={alignOffset}
+        >
+          <p className="font-semibold capitalize">{label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
