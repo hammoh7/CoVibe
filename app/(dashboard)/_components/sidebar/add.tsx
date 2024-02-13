69c15c9d62802241d69372960219e8fb39949665
hammoh7 <soahammohaadkar@gmail.com>
2024-02-14 00:41:56 +0530

    Sidebar

--- app/(dashboard)/_components/sidebar/add.tsx
+++ app/(dashboard)/_components/sidebar/add.tsx
@@ -3,15 +3,18 @@
 import { Plus } from "lucide-react";
 import { CreateOrganization } from "@clerk/nextjs";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Label } from "@/components/label";
 
 export const AddButton = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
         <div className="aspect-square">
+          <Label label="Create organization" side="right">
           <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-90 transition">
             <Plus className="text-white" />
           </button>
+          </Label>
         </div>
       </DialogTrigger>
       <DialogContent className="p-0 bg-transparent border-none">
--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,9 +1,12 @@
 import { AddButton } from "./add";
+import { List } from "./lists";
 
 export const Sidebar = () => {
     return ( 
         <div className="fixed z-[1] left-0 bg-blue-800 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">
             <AddButton />
+            <br />
+            <List />
         </div>
      );
 }
