a3596f485efd788d721d856d72152945638c7040
hammoh7 <soahammohaadkar@gmail.com>
2024-02-14 13:30:35 +0530

    Dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,30 @@
-const Dashboard = () => {
+"use client";
+
+import { useOrganization } from "@clerk/nextjs";
+import { EmptyOrg } from "./_components/main/empty-org";
+import { BoardLists } from "./_components/boards/board-list";
+
+interface DashboardProps {
+    searchParams: {
+        search?: string,
+        favorites?: string,
+    }
+}
+
+const Dashboard = ({
+    searchParams,
+}: DashboardProps) => {
+    const { organization } = useOrganization();
     return ( 
-        <div>
-            Dashboard page
+        <div className="flex-1 h-[calc(100%-98px)] p-6">
+            {!organization ? (
+                <EmptyOrg />
+            ) : (
+                <BoardLists 
+                  orgId={organization.id}
+                  query={searchParams}
+                />
+            )}
         </div>
      );
 }
