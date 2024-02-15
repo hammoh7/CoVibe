d82f6a3ee2fb729a3f27f2f2c858fcd2ea71ca6c
hammoh7 <soahammohaadkar@gmail.com>
2024-02-16 00:18:12 +0530

    Components

--- components/actions.tsx
+++ components/actions.tsx
@@ -1,27 +1,51 @@
 "use client";
 
 import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Link } from "lucide-react";
+import { toast } from "sonner";
 
 interface ActionsProps {
-    children: React.ReactNode;
-    side?: DropdownMenuContentProps["side"]
-    sideOffset?: DropdownMenuContentProps["sideOffset"]
-    id: string
-    title: string
+  children: React.ReactNode;
+  side?: DropdownMenuContentProps["side"];
+  sideOffset?: DropdownMenuContentProps["sideOffset"];
+  id: string;
+  title: string;
 }
 
 export const Actions = ({
-    children, side, sideOffset, id, title, 
+  children,
+  side,
+  sideOffset,
+  id,
+  title,
 }: ActionsProps) => {
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                {children}
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                
-            </DropdownMenuContent>
-        </DropdownMenu>
+  const onCopyLink = () => {
+    navigator.clipboard.writeText(
+        `${window.location.origin}/board/${id}`
     )
-}
+    .then(() => toast.success("Link copied"))
+    .catch(() => toast.error("Error copying link"))
+  }
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        onClick={(e) => e.stopPropagation()}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-50"
+      >
+        <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}>
+          <Link className="h-4 w-4 mr-2" />
+          Copy board link
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
