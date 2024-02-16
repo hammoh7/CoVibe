e79ab75104fc1785bcd96b069b264f3e3ab08a5f
hammoh7 <soahammohaadkar@gmail.com>
2024-02-16 21:43:14 +0530

    Convex

--- convex/board.ts
+++ convex/board.ts
@@ -1,32 +1,66 @@
-import { v } from "convex/values"
-import { mutation } from "./_generated/server" 
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
 
-const images = [
-    "./logo.svg"
-]
+const images = ["./logo.svg"];
 
 export const create = mutation({
-    args: {
-        orgId: v.string(),
-        title: v.string(),
-    },
-    handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity();
+  args: {
+    orgId: v.string(),
+    title: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
 
-        if(!identity) {
-            throw new Error ("Unauthorized")
-        }
-
-        const randomImage = images[Math.floor(Math.random() * images.length)];
-
-        const board = await ctx.db.insert("boards", {
-            title: args.title,
-            orgId: args.orgId,
-            authorId: identity.subject,
-            authorName: identity.name!,
-            imageUrl: randomImage,
-        })
-
-        return board;
+    if (!identity) {
+      throw new Error("Unauthorized");
     }
+
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+
+    const board = await ctx.db.insert("boards", {
+      title: args.title,
+      orgId: args.orgId,
+      authorId: identity.subject,
+      authorName: identity.name!,
+      imageUrl: randomImage,
+    });
+
+    return board;
+  },
+});
+
+export const remove = mutation({
+  args: { id: v.id("boards") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+    await ctx.db.delete(args.id);
+  },
+});
+
+export const update = mutation({
+  args: { id: v.id("boards"), title: v.string() },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+    
+    const title = args.title.trim();
+    if (!title) {
+      throw new Error("Title requried");
+    }
+    if (title.length > 25) {
+      throw new Error("Title cannot be more than 25 words")
+    }
+
+    const board = await ctx.db.patch(args.id, {
+      title: args.title,
+    })
+    return board;
+  }
 })
