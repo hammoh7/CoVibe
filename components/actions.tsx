4fb07d572f8d867c56bc41e102f1f0587a1f6b18
hammoh7 <soahammohaadkar@gmail.com>
2024-02-16 21:47:21 +0530

    Components

--- components/actions.tsx
+++ components/actions.tsx
@@ -7,8 +7,13 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { Link } from "lucide-react";
+import { Link, PenIcon, Trash } from "lucide-react";
 import { toast } from "sonner";
+import { apiMutation } from "@/hooks/mutation-api";
+import { api } from "@/convex/_generated/api";
+import { ConfirmModal } from "./modal-confirm";
+import { Button } from "./ui/button";
+import { renameModal } from "@/store/modal-rename";
 
 interface ActionsProps {
   children: React.ReactNode;
@@ -25,13 +30,22 @@ export const Actions = ({
   id,
   title,
 }: ActionsProps) => {
+  const { onOpen } = renameModal();
+  const { mutate, pending } = apiMutation(api.board.remove);
+
   const onCopyLink = () => {
-    navigator.clipboard.writeText(
-        `${window.location.origin}/board/${id}`
-    )
-    .then(() => toast.success("Link copied"))
-    .catch(() => toast.error("Error copying link"))
-  }
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => toast.success("Link copied"))
+      .catch(() => toast.error("Error copying link"));
+  };
+
+  const onDelete = () => {
+    mutate({ id })
+      .then(() => toast.success("Board deleted"))
+      .catch(() => toast.error("Error deleting board"));
+  };
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
@@ -45,6 +59,21 @@ export const Actions = ({
           <Link className="h-4 w-4 mr-2" />
           Copy board link
         </DropdownMenuItem>
+        <DropdownMenuItem className="p-3 cursor-pointer" onClick={() => onOpen(id, title)}>
+          <PenIcon className="h-4 w-4 mr-2" />
+          Rename
+        </DropdownMenuItem>
+        <ConfirmModal
+          header="Delete board"
+          description="This will permanently delete the board and its content"
+          disabled={pending}
+          onConfirm={onDelete}
+        >
+          <Button variant="ghost" className="p-3 cursor-pointer text-sm w-full justify-start font-normal">
+            <Trash className="h-4 w-4 mr-2" />
+            Delete
+          </Button>
+        </ConfirmModal>
       </DropdownMenuContent>
     </DropdownMenu>
   );
