c598fc87f33a4f5352fb2ac9cdb4bd0a98159074
hammoh7 <soahammohaadkar@gmail.com>
2024-02-17 17:34:14 +0530

    Library

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const colors = [
+  "#f43f5e",
+  "#fde047",
+  "#22c55e",
+  "#3b82f6",
+  "#9333ea",
+];
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function connectionIdToColor(connectionId: number): string {
+  return colors[connectionId % colors.length];
+};
