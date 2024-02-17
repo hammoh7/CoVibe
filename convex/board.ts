0f3d3c1c9f37fa515cc7e96d2e518f0cfa39ebd1
hammoh7 <soahammohaadkar@gmail.com>
2024-02-17 17:34:42 +0530

    Convex

--- convex/board.ts
+++ convex/board.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 const images = ["./logo.svg"];
 
@@ -146,3 +146,11 @@ export const notFavorite = mutation({
     return board;
   },
 });
+
+export const get = query({
+  args: { id:v.id("boards") },
+  handler: async(ctx, args) => {
+    const board = ctx.db.get(args.id);
+    return board;
+  }
+})
