fab84943e07568a7f051768e7c596b78a5f7e78a
hammoh7 <soahammohaadkar@gmail.com>
2024-02-18 11:30:49 +0530

    Library

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,19 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { Camera } from "@/types/canvas";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-const colors = [
-  "#f43f5e",
-  "#fde047",
-  "#22c55e",
-  "#3b82f6",
-  "#9333ea",
-];
+const colors = ["#f43f5e", "#fde047", "#22c55e", "#3b82f6", "#9333ea"];
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
-
 
 export function connectionIdToColor(connectionId: number): string {
   return colors[connectionId % colors.length];
-};
+}
+
+export function pointerEventToCanvas(e: React.PointerEvent, camera: Camera) {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
+}
