9ce6f8906f902d4030860bb9e9766d3466444c9b
hammoh7 <soahammohaadkar@gmail.com>
2024-02-18 11:26:26 +0530

    Canvas

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,38 +1,73 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Content } from "./content";
 import { Toolbar } from "./toolbar";
 import { Users } from "./users";
-import { CanvasMode, CanvasState } from "@/types/canvas";
-import { useCanRedo, useCanUndo, useHistory } from "@/liveblocks.config";
+import { Camera, CanvasMode, CanvasState } from "@/types/canvas";
+import {
+  useCanRedo,
+  useCanUndo,
+  useHistory,
+  useMutation,
+} from "@/liveblocks.config";
+import { UserCursor } from "./user-cursor";
+import { pointerEventToCanvas } from "@/lib/utils";
 
 interface CanvasProps {
-    boardId: string,
+  boardId: string;
 }
 
-export const Canvas = ({
-    boardId,
-}: CanvasProps) => {
-    const [canvasState, setCanvasState] = useState<CanvasState>({
-        mode: CanvasMode.None,
-    });
-    const history = useHistory();
-    const canUndo = useCanUndo();
-    const canRedo = useCanRedo();
+export const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+  const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
+  const onWheel = useCallback((e: React.WheelEvent) => {
+    setCamera((camera) => ({
+      x: camera.x - e.deltaX,
+      y: camera.y - e.deltaY,
+    }));
+  }, []);
+  const onPointerMove = useMutation(
+    ({ setMyPresence }, e: React.PointerEvent) => {
+      e.preventDefault();
 
-    return (
-        <main className="h-full w-full relative bg-slate-100 touch-none">
-            <Content boardId={boardId} />
-            <Users />
-            <Toolbar
-              canvasState={canvasState}
-              setCanvasState={setCanvasState}
-              undo={history.undo}
-              redo={history.redo}
-              canUndo={canUndo}
-              canRedo={canRedo}
-            />
-        </main>
-    )
-}
+      const current = pointerEventToCanvas(e, camera);
+      setMyPresence({ cursor: current });
+    },
+    []
+  );
+
+  const onPointerLeave = useMutation(({ setMyPresence }) => {
+    setMyPresence({ cursor: null });
+  }, []);
+
+  return (
+    <main className="h-full w-full relative bg-slate-100 touch-none">
+      <Content boardId={boardId} />
+      <Users />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        undo={history.undo}
+        redo={history.redo}
+        canUndo={canUndo}
+        canRedo={canRedo}
+      />
+      <svg
+        className="h-[100vh] w-[100vw]"
+        onWheel={onWheel}
+        onPointerMove={onPointerMove}
+        onPointerLeave={onPointerLeave}
+      >
+        <g>
+          <UserCursor />
+        </g>
+      </svg>
+    </main>
+  );
+};
