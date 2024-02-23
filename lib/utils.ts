958c665db293220e1d3c431eb5222fbc6dbd4ccc
hammoh7 <soahammohaadkar@gmail.com>
2024-02-23 22:05:41 +0530

    Library

--- lib/utils.ts
+++ lib/utils.ts
@@ -144,3 +144,18 @@ export function penPointToPath(points: number[][], color: Color): PathLayer {
   };
 }
 
+export function getSvgFromStroke(stroke: number[][]) {
+  if (!stroke.length) return "";
+
+  const d = stroke.reduce(
+    (acc, [x0, y0], i, arr) => {
+      const [x1, y1] = arr[(i + 1) % arr.length];
+      acc.push(x0, y0, (x0 + x1) / 2, (y0 + y1) / 2);
+      return acc;
+    },
+    ["M", ...stroke[0], "Q"]
+  );
+
+  d.push("Z");
+  return d.join(" ");
+}
